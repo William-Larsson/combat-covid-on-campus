4dc755de3151f4d36798f9bf97687a9afa4f6c4d
William Larsson <williaamlarsson@gmail.com>
2020-12-14 12:14:36 +0100

    Initial code for API-calls to Daresay LoRa

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 var cors = require('cors');
 
+//for mongodb connection
+const config = {
+  autoIndex: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
 const app = express();
+
+//db connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://ccoc_proj:ettan1@clustertest.q9ix0.mongodb.net/ccoc', config);
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to db'));
 
 //serves static files directly from server
 //app.use(express.static('../../client/public'));
